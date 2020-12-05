d82be39813c43391a4753d889b99f03862d625b3
Quant85 <antonio.85.q@gmail.com>
2020-12-05 20:08:21 +0100

    Raccoglitore di zucchine randomico

--- JSnacks Blocco 5/JSnack 1/main.js
+++ JSnacks Blocco 5/JSnack 1/main.js
@@ -97,3 +97,66 @@ var totalePesoZucchine = sommaPropOggInArray (boxZucchine,"peso");
 console.log(totalePesoZucchine);
 var totalePesoZucchine = sommaPropOggInArray (boxZucchine,"lunghezza");
 console.log(totalePesoZucchine);
+
+
+
+
+// Zucchine Costructor --- sol. 2.0
+
+class Zucchina {
+  constructor (a, b, c) {
+    this.varietà = a;
+    this.lunghezza = b;
+    this.peso = c;
+  }
+}
+
+var cestoZucchine = [];
+
+//Raccogliotere - prendi 10 zucchine lunghe più di 10 cm e non oltre le 34 e tra i 100gr e i 340gr
+
+var varietàZucchine = [];
+var provenienza = [" di Faenza"," di Firenze"," di Milano"," di Napoli"," di Roma"," di Sicilia"];
+var caratteristiche = [" striato corto"," striato lungo"," rigato tondo"," liscio tondo"," nero", " chiaro"];
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var zucchineRaccolte = 10;
+var lunghezzaMin = 10;
+var lunghezzaMax = 34;
+var pesoaMin = 80;
+var pesoMax = 160;
+
+while (varietàZucchine.length < zucchineRaccolte) {
+  var pesoX = getRandomNumber(pesoaMin,pesoMax);
+  //console.log(pesoX);
+  var lunghezzaY = getRandomNumber(lunghezzaMin,lunghezzaMax);
+  //console.log(lunghezzaY);
+  //--potrei separare il generatore per una miglior comprensione o creare una funzione
+  var provenienzaX = getRandomNumber(0,provenienza.length-1);
+  //console.log(provenienza[provenienzaX]);
+
+  var caratteristicheY = getRandomNumber(0,caratteristiche.length-1);
+  var varietàRandom = "Zucchino" + caratteristiche[caratteristicheY] + provenienza[provenienzaX] ;
+  //console.log(varietàRandom);
+  
+  if (varietàZucchine.length < zucchineRaccolte) {
+    varietàZucchine.push(varietàRandom);
+  }
+  var varietàZucchineX = getRandomNumber(0,varietàZucchine.length-1);
+  var zucchinaRaccolta = new Zucchina(varietàZucchine[varietàZucchineX],lunghezzaY,pesoX);
+  cestoZucchine.push(zucchinaRaccolta);
+}
+
+console.log(varietàZucchine);
+console.log(zucchinaRaccolta);
+console.log(cestoZucchine);
+
+var pesoZucchine = 0;
+cestoZucchine.forEach(function(item){
+  pesoZucchine += item.peso;
+});
+
+console.log(pesoZucchine);
